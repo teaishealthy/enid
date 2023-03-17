7914e08307b4d3ba2cba656a7de017a3604fc33c
teaishealthy <teaishealthy@protonmail.com>
2023-03-17 18:01:12 +0100

    feat: scroll new messages into view

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,11 +1,23 @@
 import { Flex, Text, useColorModeValue } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-function _Message({ content, author }: { content: string; author: string }) {
+function _Message({
+    content,
+    author,
+}: {
+    content: string;
+    author: string | undefined;
+}) {
     const elevatedBackground = useColorModeValue('gray.100', 'gray.750');
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: 'smooth' });
+    }, []);
 
     return (
         <Flex
+            ref={ref}
             flexDir="column"
             _hover={{ bg: elevatedBackground }}
             rounded={'2xl'}
