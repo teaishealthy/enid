15d8aed987eac272a3bbef3f94dc1219e43c3d07
teaishealthy <teaishealthy@protonmail.com>
2023-03-19 01:23:29 +0100

    feat: pwa!

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA }from 'vite-plugin-pwa'
+import Manifest from "./manifest.json"
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA({
+        manifest: Manifest,
+        workbox: {
+            globPatterns: [
+                '**/*.{js,css,html,png,jpg,jpeg,gif,svg,ico,webp,webmanifest}'
+            ],
+        }
+    })],
 });
