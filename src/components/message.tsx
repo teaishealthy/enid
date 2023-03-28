d3019488fbee4695a99b35f19e377b256ace2354
teaishealthy <teaishealthy@protonmail.com>
2023-03-28 23:53:12 +0200

    feat: add embedding of spotify links This uses the oEmbed specification so it can be extended to other sites later

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,6 +1,51 @@
-import { Flex, Link, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex, Link, Text, useColorModeValue } from '@chakra-ui/react';
 import Linkify from 'linkify-react';
 import React, { useEffect, useRef } from 'react';
+
+interface Embed {
+    html: string;
+    width?: number | null;
+    height?: number | null;
+    version: string;
+    provider_name: 'Spotify';
+    provider_url: 'https://spotify.com';
+    type: 'rich';
+    title: string;
+    thumbnail_url?: string | null;
+    thumbnail_width?: number | null;
+    thumbnail_height?: number | null;
+}
+
+function _SpotfiyEmbed({ url }: { url: string }) {
+    const [embed, setEmbed] = React.useState<null | Embed>(null);
+
+    React.useEffect(() => {
+        fetch(`https://open.spotify.com/oembed?url=${url}`)
+            .then((response) => response.json())
+            .then((data) => setEmbed(data));
+    }, [url]);
+
+    if (embed === null) return null;
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(embed.html, 'text/html');
+    const iframe = doc.querySelector('iframe');
+    iframe!.height = '80px';
+    iframe!.width = '70%';
+
+    return (
+        <>
+            <Link href={url} color="teal.500" isExternal>
+                {url}
+            </Link>
+            <Box
+                dangerouslySetInnerHTML={{ __html: iframe?.outerHTML ?? '' }}
+            />
+        </>
+    );
+}
+
+const SpotfiyEmbed = React.memo(_SpotfiyEmbed);
 
 const renderLink = ({
     attributes,
@@ -9,7 +54,14 @@ const renderLink = ({
     attributes: any; // eslint-disable-line @typescript-eslint/no-explicit-any
     content: any; // eslint-disable-line @typescript-eslint/no-explicit-any
 }) => {
-    const { href, ...props } = attributes;
+    const { href, ...props }: { href: string; [key: string]: any } = attributes; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+    const hrefUrl = new URL(href);
+
+    if (hrefUrl.hostname === 'open.spotify.com') {
+        return <SpotfiyEmbed url={href} />;
+    }
+
     return (
         <Link href={href} color="teal.500" {...props}>
             {content}
