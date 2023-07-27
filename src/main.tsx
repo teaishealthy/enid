5c5c9563b787bd3641fe8c0df549d47fef4eefe2
teaishealthy <teaishealthy@protonmail.com>
2023-07-27 13:00:14 +0200

    chore: lowercase file names

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './app';
 import { ThemeProvider } from './theme-context';
 
 import './index.css';
